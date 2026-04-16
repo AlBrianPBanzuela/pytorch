cdbeb736e6c8b5676fb157c0803e1a8ec383ed27
Arsh Zahed <arshzahed@meta.com>
2026-04-16 16:03:04 -0700

    [dynamo] Refactor SetAttrBuiltinVariable
    [ghstack-poisoned]

--- torch/_dynamo/variables/__init__.py
+++ torch/_dynamo/variables/__init__.py
@@ -24,6 +24,7 @@ from .builtin import (
     GetAttrBuiltinVariable,
     IterBuiltinVariable,
     ListBuiltinVariable,
+    SetAttrBuiltinVariable,
 )
 from .constant import ConstantVariable
 from .ctx_manager import (
@@ -235,6 +236,7 @@ __all__ = [
     "RepeatIteratorVariable",
     "SDPAParamsVariable",
     "ErrorOnGraphBreakVariable",
+    "SetAttrBuiltinVariable",
     "SkipFunctionVariable",
     "SliceVariable",
     "StringFormatVariable",
