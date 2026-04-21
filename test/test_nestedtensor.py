4e999194a893b66ef11e09c91d73eed63cf885a1
soulitzer <soulitzer@gmail.com>
2026-04-21 15:44:03 -0700

    Rename _maybe_detach to _detach_with_unexclude in SAC
    Previously _maybe_detach conditionally detached based on
    any_ret_has_alias_info. Since we always need the detach for version
    counter propagation and reference cycle avoidance, simplify to always
    detach any tensor. Also rename to _detach_with_unexclude to better
    describe what the function does.
    
    Authored with Claude.
    
    ghstack-source-id: c125a85fae96104b1edba6f58b344a4b4cdbe6d0
    ghstack-source-id: c125a85fae96104b1edba6f58b344a4b4cdbe6d0
    Pull-Request: https://github.com/pytorch/pytorch/pull/180919

--- test/test_nestedtensor.py
+++ test/test_nestedtensor.py
@@ -7477,22 +7477,6 @@ torch.cuda.synchronize()
         checkpoint(fn, values, offsets, use_reentrant=False).backward()
         self.assertIsNotNone(values.grad)
 
-        context_fn = partial(
-            create_selective_checkpoint_contexts, [torch.ops.aten.cumsum.default]
-        )
-
-        values.grad = None
-
-        def fn(values, lengths):
-            offsets = F.pad(lengths, pad=(1, 0)).cumsum(dim=0)
-            nt = convert_jagged_to_nested_tensor(values, offsets, max_length=4)
-            return convert_nt_to_jagged(nt).sum()
-
-        checkpoint(
-            fn, values, lengths, use_reentrant=False, context_fn=context_fn
-        ).backward()
-        self.assertIsNotNone(values.grad)
-
     # Internally-defined NT use cases are lifted to here for maximum test realism.
     # TODO: Remove these when ViewNestedFromBuffer, etc. are deprecated.
     @skipCUDAIfRocm  # not needed
